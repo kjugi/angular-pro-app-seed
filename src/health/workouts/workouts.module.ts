f59e5e1d7c17e9eaa0512752785893d261867ee5
Filip <filip.kudla@snow.dog>
2020-09-22 18:15:02 +0200

    Update store with workouts value + update shared module with workouts service

--- src/health/workouts/workouts.module.ts
+++ src/health/workouts/workouts.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { WorkoutsComponent } from './containers/workouts/workouts.component'
+import { WorkoutComponent } from './containers/workout/workout.component'
+
+import { WorkoutFormComponent } from './components/workout-form/workout-form.component'
+import { WorkoutTypeComponent } from './components/workout-type/workout-type.component'
 
 export const ROUTES: Routes = [
   {
     path: '',
     component: WorkoutsComponent
+  },
+  {
+    path: 'new',
+    component: WorkoutComponent
+  },
+  {
+    path: ':id',
+    component: WorkoutComponent
   }
 ]
 
@@ -16,10 +30,14 @@ export const ROUTES: Routes = [
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    SharedModule
   ],
   declarations: [
-    WorkoutsComponent
+    WorkoutsComponent,
+    WorkoutComponent,
+    WorkoutFormComponent,
+    WorkoutTypeComponent
   ]
 })
 export class WorkoutsModule {}
