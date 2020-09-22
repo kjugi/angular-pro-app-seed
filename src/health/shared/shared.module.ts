e2365063bf61ca78659718aa459351e9d43d3520
Filip <filip.kudla@snow.dog>
2020-09-22 12:13:32 +0200

    Add meals to store and create service to get meals

---
+++ src/health/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { AngularFireDatabaseModule } from 'angularfire2/database'
+
+import { MealsService } from './services/meals/meals.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    AngularFireDatabaseModule
+  ],
+  declarations: []
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [
+        MealsService
+      ]
+    }
+  }
+}
