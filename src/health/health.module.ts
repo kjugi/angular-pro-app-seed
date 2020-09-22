10abb25f1e4310d33fdd017c9ff32f81949addad
Filip <filip.kudla@snow.dog>
2020-09-22 11:56:28 +0200

    Add guard for shared,meals,workouts routes

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,24 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SharedModule } from './shared/shared.module';
+
+import { AuthGuard } from '../auth/shared/guards/auth.guard'
+
 export const ROUTES: Routes = [
   {
     path: 'meals',
+    canActivate: [
+      AuthGuard
+    ],
     loadChildren: './meals/meals.module#MealsModule'
   },
   {
     path: 'schedule',
+    canActivate: [
+      AuthGuard
+    ],
     loadChildren: './schedule/schedule.module#ScheduleModule'
   },
   {
     path: 'workouts',
+    canActivate: [
+      AuthGuard
+    ],
     loadChildren: './workouts/workouts.module#WorkoutsModule'
   }
 ]
 
 @NgModule({
   imports: [
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    SharedModule.forRoot()
   ]
 })
 export class HealthModule {}
