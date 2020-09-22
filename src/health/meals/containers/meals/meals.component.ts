c3aca6a84bb3251317cb6e109d8472243cba6e12
Filip <filip.kudla@snow.dog>
2020-09-22 12:14:27 +0200

    Get meals in meals.component

--- src/health/meals/containers/meals/meals.component.ts
+++ src/health/meals/containers/meals/meals.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Store } from 'store';
+
+import { Meal, MealsService } from '../../../shared/services/meals/meals.service';
 
 @Component({
   selector: 'meals',
   styleUrls: ['meals.component.scss'],
   template: `
     <div>
-      Meals
+      {{ meals$ | async | json}}
     </div>
   `
 })
-export class MealsComponent {
-  constructor() {}
+export class MealsComponent implements OnInit, OnDestroy {
+  meals$: Observable<Meal[]>
+  subscription: Subscription
+  constructor(
+    private mealsService: MealsService,
+    private store: Store
+  ) {}
+
+  ngOnInit() {
+    this.meals$ = this.store.select<Meal[]>('meals')
+    this.subscription = this.mealsService.meals$.subscribe()
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
 }
